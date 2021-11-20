9537fcaee02f7ead3f0f11627e0d4522facdf46d
Eslam Abdelbasset <eslamelbazedy@gmail.com>
2021-11-21 00:53:22 +0200

    Start project

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::group(['middleware' => 'api'], function (){
+Route::group(['middleware' => ['api', 'checkPassword'],'namespace' => 'Api'], function (){
     Route::post('get-main-categories', [CategoriesController::class, 'index']);
 });
 
