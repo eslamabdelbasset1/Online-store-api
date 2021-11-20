bfa8c1215e7c39c49ff2d972d4a3a5185f479095
Eslam Abdelbasset <eslamelbazedy@gmail.com>
2021-11-20 23:58:36 +0200

    Start project

--- app/Http/Controllers/api/CategoriesController.php
+++ app/Http/Controllers/api/CategoriesController.php
@@ -3,9 +3,14 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoriesController extends Controller
 {
-    //
+    public function index()
+    {
+        $categories = Category::get();
+        return response()->json($categories);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::group(['middleware' => 'auth:api'], function (){
+Route::group(['middleware' => 'api'], function (){
     Route::post('get-main-categories', [CategoriesController::class, 'index']);
 });
+
